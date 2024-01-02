c43adccdd92ce25d7f3d591b094bb5b806aa771f
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-01-02 16:59:06 +0600

    add 2 file docker & flatpack

---
+++ docker-setup.sh
@@ -0,0 +1,10 @@
+sudo apt update
+
+sudo apt install -y docker.io
+
+sudo systemctl enable docker --now
+
+sudo usermod -aG docker $USER
+
+docker
+
---
+++ flatpack.sh
@@ -0,0 +1,20 @@
+sudo apt update
+
+sudo apt install -y flatpak
+
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+
+sudo apt install gnome-software-plugin-flatpak
+
+
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+
+sudo apt update
+
+sudo apt install -y tor torbrowser-launcher
+
+torbrowser-launcher
+
+
