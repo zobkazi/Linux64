bf2440c1bf54e9edbf4a66559c45c90fcc9828da
Zobaidul Kazi <105772384+zobaidulkazi64@users.noreply.github.com>
2024-03-25 04:12:52 +0600

    Create kill.sh

---
+++ kill.sh
@@ -0,0 +1,8 @@
+## The 'kill' command in Linux is used to send a signal to a process, typically to terminate it. You can use it by specifying the Process ID (PID) of the process you want to terminate, like this: kill 1234
+
+
+kill [signal] PID
+
+kill -9 1212
+
+kill pid
