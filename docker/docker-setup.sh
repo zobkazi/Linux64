f5b31beb739b6d3d1d8f442d5f03442e357a755a
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-06-13 01:18:27 +0600

    docker success

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -1,10 +1,43 @@
-sudo apt update
+## #! docker-setup.sh
 
-sudo apt install -y docker.io
+## Set Configurations For Docker
 
-sudo systemctl enable docker --now
+#!/bin/sh
+echo "Docker Configurations Edite Power by @zobaidulkazi"
 
-sudo usermod -aG docker $USER
+echo "https://github.com/zobkazi"
 
-docker
+echo "https://github.com/zobkazi/Linux64/blob/main/README.md"
 
+# Check if Docker is already installed
+if command -v docker &>/dev/null; then
+    echo "Docker is already installed. Exiting."
+    exit 0
+fi
+
+echo "Install Docker......... Y/N"
+
+read y_n
+
+if [ "$y_n" = "Y" ] || [ "$y_n" = "y" ]; then
+    sudo apt install docker.io
+
+    sudo systemctl start docker
+
+    sudo systemctl enable docker
+
+    sudo usermod -aG docker $USER
+
+    echo "Docker Install Successful........."
+
+    echo "Please Exit Your Terminal And Reopen It........"
+
+    echo "Thank You, Bye..!! :)"
+
+    exit
+
+fi
+
+echo "Thank You, Bye..!! :)"
+
+exit
