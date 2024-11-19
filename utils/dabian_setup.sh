c07ea3ec151423238af9e9456e942894b9145c6f
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2024-11-20 01:47:48 +0600

    dabian liux default setup

---
+++ utils/dabian_setup.sh
@@ -0,0 +1,25 @@
+su
+
+nano /etc/sudoers
+
+zobaidul ALL = (ALL : ALL) ALL
+
+
+exit
+
+# Groub Time
+sudo nano /etc/default/grub
+
+# set time
+Groub Time = 0
+
+# groub Update
+sudo update-grub
+
+
+
+
+## host name ch
+
+sudo nano /etc/hostname
+
