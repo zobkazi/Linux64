3b01004a069b2b2e96d0267a1f2f29e05f5de89e
Zobaidul Kazi <zobaidulkazi64.official@gmail.com>
2025-03-06 10:08:04 +0600

    add dnf gnome pakages

---
+++ apps/dnf.sh
@@ -0,0 +1,3 @@
+# gnome all apps install
+
+sudo dnf install gnome-2048 gnome-clocks gnome-color-manager gnome-extensions-app gnome-photos gnome-terminal gnome-todo gnome-tweaks google-chrome-stable
